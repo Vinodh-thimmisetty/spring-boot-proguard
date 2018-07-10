a474586d1569e5a485b22c704c199a793e4ebb85
vinodh kumar Thimmisetty <vinodh5052@gmail.com>
2018-07-11 01:30:32 +0530

    test

---
+++ src/main/java/com/vinodh/SpringBootProguardApplication.java
@@ -0,0 +1,12 @@
+package com.vinodh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProguardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootProguardApplication.class, args);
+	}
+}
